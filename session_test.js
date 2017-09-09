581f38b998a9446d054d7d6848c8b22aab56f930
Otto <otto.christianto.oc@gmail.com>
2017-09-09 11:59:24 +0700

    success apply session

--- session_test.js
+++ session_test.js
@@ -1,24 +1,73 @@
-var express = require('express');
-var session = require('express-session')
+http = require('http')
+portNumb = 1775
 
+var path = require('path')
+var express = require('express')
+var session = require('express-session');
+var exhbs = require('express-handlebars')
+var bodyParser = require('body-parser')
 var app = express();
 
-app.use(session({
-    secret: "Shh, its a secret!",
-    cookie: {
-        maxAge: 60000
-    }
-}));
+app.engine('.hbs', exhbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}))
 
-app.get('/', function(req, res){
-   if(req.session.page_views){
-      req.session.page_views++;
-      res.cookie('test', 'yes')
-      console.log(res.cookies.test)
-      res.send("You visited this page " + req.session.page_views + " times");
-   } else {
-      req.session.page_views = 1;
-      res.send("Welcome to this page for the first time!");
-   }
+app.set('view engine', '.hbs')
+
+app.use(session({secret: 'ssshhh'}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+
+var sess;
+
+app.get('/', (req, res) => {
+    sess = req.session;
+    if(sess.email) {
+        console.log('find email')
+        res.redirect('/admin')
+    }
+    else {
+        console.log('no email')
+        res.render('index')
+    }
+})
+
+app.get('/admin',function(req,res){
+  sess = req.session;
+if(sess.email) {
+res.write('<h1>Hello ' + sess.email + '</h1>');
+res.end('<a href="/logout">Logout</a>');
+} else {
+    res.write('<h1>Please login first.</h1>');
+    res.end('<a href="/login">Login</a>');
+}
 });
-app.listen(3000);
+
+app.post('/login',function(req,res){
+    console.log("posted")
+  sess = req.session;
+//In this we are assigning email to sess.email variable.
+//email comes from HTML page.
+  sess.email=req.body.email;
+  res.end('done');
+});
+
+
+app.get('/logout',function(req,res){
+req.session.destroy(function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    res.redirect('/');
+  }
+});
+});
+
+app.listen(portNumb, (err) => {
+    if(err) {
+        return console.log('something bad happned', err)
+    }
+    console.log('server is listening on ' + portNumb)
+})
